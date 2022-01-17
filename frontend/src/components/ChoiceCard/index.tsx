7b28637fc0e652306a00e4c7cca11dd27d30505c
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2022-01-17 18:36:56 -0300

    teste da lib de scroll animation

--- frontend/src/components/ChoiceCard/index.tsx
+++ frontend/src/components/ChoiceCard/index.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-type ChoiceCardProps = {};
+import "./styles.css";
 
-export default function ChoiceCard({}: ChoiceCardProps) {
-  return <div></div>;
+const AmazoniaGood = require("../../assets/icons/Amazonia-Good.svg");
+const AmazoniaBad = require("../../assets/icons/Amazonia-Bad.svg");
+
+type ChoiceCardProps = {
+  route: string;
+};
+
+export default function ChoiceCard({ route }: ChoiceCardProps) {
+  return (
+    <div id="ChoiceCard">
+      <div className="card">
+        <div className={`container-img container-img-${route}`}>
+          <img
+            className={"card-img"}
+            // src={
+            //   route === "goodside" ? AmazoniaGood.default : AmazoniaBad.default
+            // }
+          />
+        </div>
+        {/*
+          // TODO: esse link será um componente
+        */}
+        <div className={`container-button`}>
+          <Link
+            to={`/${route}`}
+            className={`nav-button nav-button-${route}`}
+          >
+            INICIAR
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/screens/Home/index.tsx
+++ frontend/src/screens/Home/index.tsx
@@ -1,24 +1,68 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Icon from "@mdi/react";
+import {
+  Animator,
+  ScrollContainer,
+  ScrollPage,
+  batch,
+  Fade,
+  FadeIn,
+  Move,
+  MoveIn,
+  MoveOut,
+  Sticky,
+  StickyIn,
+  ZoomIn,
+} from "react-scroll-motion";
 
+import ChoiceCard from "../../components/ChoiceCard";
+
+import "aos/dist/aos.css";
 import "./styles.css";
+
+import { mdiMouseMoveDown } from "@mdi/js";
+
+const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+const FadeUp = batch(Fade(), Move(), Sticky());
+
+const logo = require("../../assets/icons/RasgadosLogo.svg");
 
 type HomeProps = {};
 
 export default function Home({}: HomeProps) {
   return (
-    <div id="Home">
-      {/* <nav>
-        <Link to="/">Home</Link>
-        {"  "}
-        <Link to="/goodside">goodside</Link>
-        {"  "}
-        <Link to="/badside">badside</Link>
-      </nav> */}
-      <div id="HomeTitle">
-        <div className="container">{/* <img src=""/> */}</div>
-        <div className="begin-tour"></div>
-      </div>
+    <div className="home-background">
+      <ScrollContainer>
+        <ScrollPage page={0}>
+          <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -300))}>
+            <div className="flex-center flex-col">
+              <div className="title-box container flex-center flex-col">
+                <img src={logo.default} width={700} />
+                <p>
+                  um tour pela fauna em <br /> risco do brasil
+                </p>
+              </div>
+              <div className="begin-tour flex-center flex-col">
+                <p>iniciar tour</p>
+                <Icon
+                  path={mdiMouseMoveDown}
+                  title="MouseDown"
+                  size={2}
+                  color="white"
+                />
+              </div>
+            </div>
+          </Animator>
+        </ScrollPage>
+        <ScrollPage page={1}>
+          <Animator animation={FadeUp}>
+            <div style={{ display: "flex"}}>
+              <ChoiceCard route="goodside" />
+              <ChoiceCard route="badside" />
+            </div>
+          </Animator>
+        </ScrollPage>
+      </ScrollContainer>
     </div>
   );
 }
