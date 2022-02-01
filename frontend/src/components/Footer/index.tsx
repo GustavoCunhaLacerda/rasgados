ab41d9a1c4f29dc6c973087746373171614d400a
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2022-02-01 17:25:29 -0300

    melhorando o footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,4 +1,6 @@
 import styles from './styles.module.scss';
+import { Icon } from '@mdi/react';
+import { mdiGithub } from '@mdi/js';
 
 export default function Footer() {
   return (
@@ -15,14 +17,32 @@ export default function Footer() {
         <div className={styles.sectionsContainer}>
           <div className={styles.section}>
             <h5>Integrantes</h5>
-            <span>Cinthia Mie Ungefehr Nagahama</span>
-            <span>Gustavo Cunha Lacerda</span>
-            <span>Henrique Tavares Aguiar</span>
+            <a href='https://github.com/CinthiaNagahama' target='_blank' rel='noreferrer' className={styles.link}>
+              <Icon path={mdiGithub} size={1} style={{ marginRight: 5 }} /> Cinthia Mie Ungefehr Nagahama
+            </a>
+            <a href='https://github.com/GustavoCunhaLacerda' target='_blank' rel='noreferrer' className={styles.link}>
+              <Icon path={mdiGithub} size={1} style={{ marginRight: 5 }} /> Gustavo Cunha Lacerda
+            </a>
+            <a href='https://github.com/henrique-tavares' target='_blank' rel='noreferrer' className={styles.link}>
+              <Icon path={mdiGithub} size={1} style={{ marginRight: 5 }} /> Henrique Tavares Aguiar
+            </a>
           </div>
           <div className={styles.section}>
             <h5>Referências</h5>
-            <span>ICMBio</span>
-            <span>WikiAves</span>
+            <a
+              href='https://www.icmbio.gov.br/portal/faunabrasileira/planos-de-reducao-de-impacto/7000-uncategorised/10187-livro-vermelho-ed-ano-2018'
+              target='_blank'
+              rel='noreferrer'
+              className={styles.link}
+            >
+              ICMBio - Livro Vermelho - ed. ano 2018
+            </a>
+            <a href='https://www.wikiaves.com.br/' target='_blank' rel='noreferrer' className={styles.link}>
+              WikiAves
+            </a>
+            <a href='https://www.iucnredlist.org/' target='_blank' rel='noreferrer' className={styles.link}>
+              IUCN - Red List
+            </a>
           </div>
         </div>
       </div>
